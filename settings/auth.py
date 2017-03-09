2860df4d107dcf2d9838559d3fba391c3be152e0
Patrick Mazulo <pmazulo@gmail.com>
2017-03-09 16:33:00 -0300

    Add facebook provider configuration

--- settings/auth.py
+++ settings/auth.py
@@ -32,4 +32,25 @@ class AuthenticationSettings:
                 'access_type': 'online',
             }
         },
+        'facebook': {
+            'METHOD': 'oauth2',
+            'SCOPE': ['email', 'public_profile', 'user_friends'],
+            'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+            'FIELDS': [
+                'id',
+                'email',
+                'name',
+                'first_name',
+                'last_name',
+                'verified',
+                'locale',
+                'timezone',
+                'link',
+                'gender',
+                'updated_time',
+                'friends',
+            ],
+            'VERIFIED_EMAIL': True,
+            'VERSION': 'v2.4',
+        }
     }
